dee063ce0cf900d70a9ac909a4954c515351a102
DogukanTekin <95180909+DogukanTekin@users.noreply.github.com>
2022-04-06 02:05:16 +0300

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,296 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.VScrollBar1 = New System.Windows.Forms.VScrollBar()
+        Me.VScrollBar2 = New System.Windows.Forms.VScrollBar()
+        Me.VScrollBar3 = New System.Windows.Forms.VScrollBar()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ListView2 = New System.Windows.Forms.ListView()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(12, 230)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(158, 124)
+        Me.CheckedListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(61, 201)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(12, 399)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(600, 230)
+        Me.ListView1.TabIndex = 2
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(61, 370)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'VScrollBar1
+        '
+        Me.VScrollBar1.Location = New System.Drawing.Point(195, 19)
+        Me.VScrollBar1.Name = "VScrollBar1"
+        Me.VScrollBar1.Size = New System.Drawing.Size(17, 80)
+        Me.VScrollBar1.TabIndex = 4
+        '
+        'VScrollBar2
+        '
+        Me.VScrollBar2.Location = New System.Drawing.Point(238, 19)
+        Me.VScrollBar2.Name = "VScrollBar2"
+        Me.VScrollBar2.Size = New System.Drawing.Size(17, 80)
+        Me.VScrollBar2.TabIndex = 5
+        '
+        'VScrollBar3
+        '
+        Me.VScrollBar3.Location = New System.Drawing.Point(285, 19)
+        Me.VScrollBar3.Name = "VScrollBar3"
+        Me.VScrollBar3.Size = New System.Drawing.Size(17, 80)
+        Me.VScrollBar3.TabIndex = 6
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(211, 102)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(26, 19)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 8
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(26, 54)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 9
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(26, 93)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(53, 125)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Label1"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(45, 141)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(111, 86)
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(74, 11)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 14
+        Me.Button4.Text = "EKLE"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ListView2
+        '
+        Me.ListView2.HideSelection = False
+        Me.ListView2.Location = New System.Drawing.Point(25, 40)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.Size = New System.Drawing.Size(600, 230)
+        Me.ListView2.TabIndex = 13
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(184, 11)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 15
+        Me.Button5.Text = "KayıtSayı"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(294, 11)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 16
+        Me.Button6.Text = "SİL"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(119, 39)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 17
+        Me.Button7.Text = "LİNK SORU"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(13, 42)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 18
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button7)
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 30)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 91)
+        Me.GroupBox1.TabIndex = 19
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Link Sorusu"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Controls.Add(Me.VScrollBar1)
+        Me.GroupBox2.Controls.Add(Me.VScrollBar2)
+        Me.GroupBox2.Controls.Add(Me.VScrollBar3)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Controls.Add(Me.PictureBox1)
+        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(244, 30)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(336, 324)
+        Me.GroupBox2.TabIndex = 20
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Telefon Sorusu"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.ListView2)
+        Me.GroupBox3.Controls.Add(Me.Button4)
+        Me.GroupBox3.Controls.Add(Me.Button5)
+        Me.GroupBox3.Controls.Add(Me.Button6)
+        Me.GroupBox3.Location = New System.Drawing.Point(629, 30)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(650, 276)
+        Me.GroupBox3.TabIndex = 21
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Soru 2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1459, 641)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Button2 As Button
+    Friend WithEvents VScrollBar1 As VScrollBar
+    Friend WithEvents VScrollBar2 As VScrollBar
+    Friend WithEvents VScrollBar3 As VScrollBar
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ListView2 As ListView
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,161 @@
+﻿Public Class Form1
+    Dim ders_kodu(), ders_adi(), ders_akts(), ders_kredi(), ders_saati(), ders_hocasi() As String
+    Dim sayi1, sayi2, sayi3 As Integer
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If ListView2.SelectedItems.Count = 0 Then
+            MsgBox("Silmek İstediğiniz Öğrencileri Seçiniz")
+        Else
+            For i As Integer = ListView2.SelectedItems.Count - 1 To 0 Step -1
+                ListView2.Items.Remove(ListView2.SelectedItems(i))
+            Next
+            MsgBox("Silindi")
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        System.Diagnostics.Process.Start(TextBox4.Text)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        MsgBox("Kayıt Sayısı : " + ListView2.Items.Count.ToString())
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim veri As New ListViewItem
+        veri = New ListViewItem(InputBox("Okul No Giriniz"))
+        veri.SubItems.Add(InputBox("Ad Giriniz"))
+        veri.SubItems.Add(InputBox("Soyad Giriniz"))
+        veri.SubItems.Add(InputBox("TC No Giriniz"))
+        veri.SubItems.Add(InputBox("Sınıf Giriniz"))
+        veri.SubItems.Add(InputBox("Kayıt Yılı Giriniz"))
+        ListView2.Items.Add(veri)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim rnd As New Random
+        Dim tek As Boolean = False
+        Do While (tek = False)
+            sayi1 = rnd.Next(1, 5)
+            If sayi1 Mod 2 = 1 Then
+                tek = True
+            End If
+        Loop
+        tek = False
+        Do While (tek = False)
+            sayi2 = rnd.Next(1, 5)
+            If sayi2 Mod 2 = 1 Then
+                tek = True
+            End If
+        Loop
+        tek = False
+        Do While (tek = False)
+            sayi3 = rnd.Next(1, 5)
+            If sayi3 Mod 2 = 1 Then
+                tek = True
+            End If
+        Loop
+        VScrollBar1.Value = sayi1
+        VScrollBar2.Value = sayi2
+        VScrollBar3.Value = sayi3
+        TextBox1.Text = sayi1.ToString()
+        TextBox2.Text = sayi2.ToString()
+        TextBox3.Text = sayi3.ToString()
+        If sayi1 = sayi2 And sayi1 = sayi3 Then
+            Label1.Text = "Tebrikler. Telefon Kazandınız"
+            PictureBox1.Image = Image.FromFile(Application.StartupPath + "\kazandin.jpg")
+            PictureBox1.Width = 150
+            PictureBox1.Height = 150
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        Else
+            PictureBox1.Width = 0
+            PictureBox1.Height = 0
+            Label1.Text = "Tekrar Deneyin"
+        End If
+    End Sub
+
+    Dim sayac As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListView2.Sorting = SortOrder.Ascending
+        ListView2.View = View.Details
+        ListView2.FullRowSelect = True
+        ListView2.GridLines = True
+        ListView2.Columns.Add("Okul No", 100)
+        ListView2.Columns.Add("Ad", 100)
+        ListView2.Columns.Add("Soyad", 100)
+        ListView2.Columns.Add("TC No", 100)
+        ListView2.Columns.Add("Sınıf", 100)
+        ListView2.Columns.Add("Kayıt Yılı", 100)
+
+        Label1.Text = "Tekrar Deneyin"
+
+        VScrollBar1.Minimum = 1
+        VScrollBar2.Minimum = 1
+        VScrollBar3.Minimum = 1
+        VScrollBar1.Maximum = 100
+        VScrollBar2.Maximum = 100
+        VScrollBar3.Maximum = 100
+
+        ListView1.View = View.Details
+        ListView1.FullRowSelect = True
+        ListView1.GridLines = True
+        ListView1.Columns.Add("Ders Kodu", 100)
+        ListView1.Columns.Add("Ders Adı", 100)
+        ListView1.Columns.Add("Ders AKTS", 100)
+        ListView1.Columns.Add("Ders Kredi", 100)
+        ListView1.Columns.Add("Ders Saati", 100)
+        ListView1.Columns.Add("Ders Hocası", 100)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        sayac += 1
+        Dim ders_ad As String = InputBox("Eklemek İstediğiniz Dersin Adını Giriniz")
+        If CheckedListBox1.Items.Contains(ders_ad) Then
+            MsgBox("Kayıtlı Ders Girişi Yaptınız")
+        Else
+            CheckedListBox1.Items.Add(ders_ad)
+            ReDim Preserve ders_kodu(sayac - 1)
+            ders_kodu(sayac - 1) = InputBox("Dersin Kodu")
+            ReDim Preserve ders_adi(sayac - 1)
+            ders_adi(sayac - 1) = ders_ad
+            ReDim Preserve ders_akts(sayac - 1)
+            ders_akts(sayac - 1) = InputBox("Dersin AKTS'si")
+            ReDim Preserve ders_kredi(sayac - 1)
+            ders_kredi(sayac - 1) = InputBox("Dersin Kredisi")
+            ReDim Preserve ders_saati(sayac - 1)
+            ders_saati(sayac - 1) = InputBox("Haftalık Ders Saati")
+            ReDim Preserve ders_hocasi(sayac - 1)
+            ders_hocasi(sayac - 1) = InputBox("Dersin Hocası")
+            MsgBox("Kayıt Başarılı")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If CheckedListBox1.CheckedItems.Count = 0 Then
+            MsgBox("Seçim Yapmadınız")
+        Else
+            For i As Integer = 0 To CheckedListBox1.CheckedItems.Count - 1
+                Dim x, y, z As Integer
+                Dim bul As Boolean = False
+                Do While (bul = False)
+                    For y = 0 To ders_adi.Length - 1
+                        If CheckedListBox1.CheckedItems(i).Equals(ders_adi(y)) Then
+                            z = y
+                            bul = True
+                        End If
+                    Next
+                Loop
+                Dim veri As ListViewItem = New ListViewItem()
+                veri = New ListViewItem(ders_kodu(z))
+                veri.SubItems.Add(ders_adi(z))
+                veri.SubItems.Add(ders_akts(z))
+                veri.SubItems.Add(ders_kredi(z))
+                veri.SubItems.Add(ders_saati(z))
+                veri.SubItems.Add(ders_hocasi(z))
+                ListView1.Items.Add(veri)
+            Next
+            MsgBox("İşlem Başarılı")
+        End If
+    End Sub
+End Class
